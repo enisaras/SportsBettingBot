790dd4f7cdc023a3f7931c55ea92dda2ab7c4f32
enisaras <arass@rpi.edu>
2021-01-15 13:54:52 -0500

    example-odds.json

--- commands/bet.js
+++ commands/bet.js
@@ -2,11 +2,24 @@
 following the format: !bet {team} {bet_type} {coins}
 */
 const { prefix } = require('../config.json'); 
-
+const data = require('../example-odds.json');
+var profit = -1;
 const team_names = ["Giants", "Jets", "Ravens", "Rams", "Packers"]; 
 
-const bet_types = ["Totals", "Teasers", "Parlays", "Point Spreads"];
-
+const bet_types = ["Totals", "Money Line", "Spread"];
+const calculateMoneyLine = (stake, odds) => {
+    //moneyline payout for positive and negative odds:
+    //TODO:add if statement to check if odds pos or neg
+    if(data['sites']['odds']['h2h'][0].Math.sign() == 1){
+        profit = stake * (odds/100);}
+    else if(data['sites']['odds']['h2h'][1].Math.sign() == 1){
+        profit = stake / (odds/100);
+    }
+    else{
+        //Needs to message "something went terribly wrong."
+        return;
+    }
+}
 module.exports = {
     name: 'bet', 
     description: 'Make a bet', 
@@ -18,16 +31,17 @@ module.exports = {
         const coins = args[2];
 
         if(!team_names.includes(team_name)) {
-            message.reply('Your team name is invalid. Please, submit a valid team name to !bet.');
+            message.reply('Your team name is invalid. Please, submit a valid team name to bet.');
         }
         if(!bet_types.includes(bet_type)) {
-            message.reply('Your bet type is invalid. Please, submit a valid bet type to !bet.'); 
+            message.reply('Your bet type is invalid. Please, submit a valid bet type to bet.'); 
         }
         if(coins < 0) {
-            message.reply('The amount of coins in your bet is invalid. Please, submit a coin amount > 0 to !bet.');
+            message.reply('The amount of coins in your bet is invalid. Please, submit a coin amount over 0 to bet.');
         }
         if(team_names.includes(team_name) && bet_types.includes(bet_type) && coins > 0){
-            message.reply(`Your bet of ${coins} coins has been added to bet: ${bet_type}`); 
+            message.reply(`Your bet of ${coins} coins has been added to bet: ${bet_type}`);
+            //message.reply(`test: ${calculateMoneyLine(coins,data['sites']['odds']['h2h'][0])}`) 
         }
     }, 
 };
