fd7fe17658fb0c3fe4f95bd5386e222544914f6e
Enis <arass@rpi.edu>
2021-01-20 14:49:14 -0500

    fixing repo issue

--- dbInitialize.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-
-// Open the database
-let db = new sqlite3.Database('./db/bettingbot.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the bettingbot database.');
-});
