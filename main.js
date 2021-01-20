bdf5a18bf753ba8e48d456e709afed89fd5da1d9
enisaras <71161399+enisaras@users.noreply.github.com>
2021-01-20 15:13:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -16,14 +16,9 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', async () => {
-<<<<<<< HEAD
-	const storedBalances = await Users.findAll();
-	storedBalances.forEach(b => currency.set(b.user_id, b));
 	console.log(`Logged in as ${client.user.id}!`);
-=======
 	console.log(`Logged in as ${client.user.tag}!`);
   console.log(process.env.REPLIT_DB_URL)
->>>>>>> 5c301899a5529394965c9e4877967ba24a700697
 });
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
